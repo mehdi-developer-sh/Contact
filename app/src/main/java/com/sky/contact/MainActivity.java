d855a24610cb116306dabe04c51b9665fd9562fe
programming <mehdi.developer.sh@gmail.com>
2021-02-15 10:18:26 +0330

    create MainActivity UI and create references fields in MainActivity.java and so on

--- app/src/main/java/com/sky/contact/MainActivity.java
+++ app/src/main/java/com/sky/contact/MainActivity.java
@@ -1,14 +1,42 @@
 package com.sky.contact;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private TextView tvNotingFound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+    }
+
+    private void findViews() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        tvNotingFound = findViewById(R.id.tvNotingFound);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void addContactBtnClick(View view) {
+
     }
 }
