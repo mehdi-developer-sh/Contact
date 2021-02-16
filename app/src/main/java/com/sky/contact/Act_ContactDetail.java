88d6580b8c0acbed4901da218d75f8a41ec96802
programming <mehdi.developer.sh@gmail.com>
2021-02-16 14:58:03 +0330

    created Act_ContactDetail

---
+++ app/src/main/java/com/sky/contact/Act_ContactDetail.java
@@ -0,0 +1,14 @@
+package com.sky.contact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act_ContactDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_contact_detail);
+    }
+}
