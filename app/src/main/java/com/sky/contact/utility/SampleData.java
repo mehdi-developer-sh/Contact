4b9ce590a448dbf487760882d2a1019ee427567c
programming <mehdi.developer.sh@gmail.com>
2021-02-16 14:45:01 +0330

    deleted for loop in SampleData class

--- app/src/main/java/com/sky/contact/utility/SampleData.java
+++ app/src/main/java/com/sky/contact/utility/SampleData.java
@@ -9,14 +9,14 @@ public class SampleData {
 
     public static List<Contact> getContacts() {
         List<Contact> contacts = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            contacts.add(new Contact(i, "Mehdi", "Khosravi" + i,
-                    "09172185392", "mehdidev78@gmail.com"));
-            contacts.add(new Contact(i, "Mina", "Mohammadi" + i,
-                    "09172185375", "mina@gmail.com"));
-            contacts.add(new Contact(i, "Maryam", "Rasooli" + i,
-                    "09172185784", "maryam@gmail.com"));
-        }
+
+        contacts.add(new Contact(0, "Mehdi", "Khosravi",
+                "09172185392", "mehdidev78@gmail.com"));
+        contacts.add(new Contact(1, "Ali", "Mohammadi",
+                "09172185375", "mina@gmail.com"));
+        contacts.add(new Contact(2, "Reza", "Rasooli",
+                "09172185784", "maryam@gmail.com"));
+
         return contacts;
     }
 }
